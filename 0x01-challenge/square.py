0da5e3cd6edc4d208f68f9b303b8223316aec542
mooreArrqs <amandanxumalo921@gmail.com>
2024-05-10 13:24:16 +0000

    square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
 class Square():
-    
-    width = 0
-    height = 0
 
-    
     def __init__(self, width=0, height=0, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
