fef837ff7a44c1b76911d875a233cf9e2f3c8330
mooreArrqs <amandanxumalo921@gmail.com>
2024-05-16 21:36:40 +0000

    My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""A square"""
 
 class Square():
 
+    """Define a class Square"""
+   
     def __init__(self, width=0, height=0):
      self.width = width
      self.height = height
